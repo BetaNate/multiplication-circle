f3ff20b53a0105e04e7331d4dec1c114f005c467
Nathan Rowe <nrowe1@unm.edu>
2023-08-29 21:51:08 -0600

    Add control listeners

--- src/timesTable.java
+++ src/timesTable.java
@@ -2,42 +2,114 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 import javafx.animation.AnimationTimer;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import java.util.ArrayList;
+import javafx.scene.layout.Pane;
 
 public class timesTable {
-    
-    final Slider tableSize = new Slider(2,360,2);
-    final Slider points = new Slider(0,360,5);
 
-    final Label tableCaption = new Label ("Times Table Value:");
-    final Label pointCaption = new Label ("Number of Points:");
 
-    final Label tableValue = new Label (Double.toString(tableSize.getValue()));
-    final Label pointValue = new Label (Double.toString(points.getValue()));
+    ArrayList<Circle> points = new ArrayList<>();
+    ArrayList<Line> lines = new ArrayList<>();
 
-    final static Color textColor = Color.WHITE;
+    public ArrayList<Circle> createCircle(Pane drawing, int pointAmt) {
 
-    int size = (int)tableSize.getValue();
-
-    private int[][] generateTable(int size) {
-        int[][] timesArray = new int[size][size];
-        
-
-        for (int row = 0; row < timesArray.length; row++) {
-            for (int col = 0; col < timesArray[row].length; col++) {
-                timesArray[row][col] = (row+1)*(col+1);
-            }
+        if (points.isEmpty() == false) {
+            points.clear();
         }
-        return timesArray; 
+
+        double centerX = drawing.getWidth() / 2;
+        double centerY = drawing.getHeight() / 2;
+
+        int pointRadius = 1;
+        int circleRadius = 150;
+
+        Circle circle = new Circle(centerX, centerY, circleRadius);
+        circle.setStroke(Color.WHITE);
+
+        drawing.getChildren().add(circle);
+
+        for(int i = 0; i < pointAmt; i++) {
+            double degrees = 360 * i / pointAmt;
+            double angle = Math.toRadians(degrees);
+
+            double xOffset = centerX - (Math.cos(angle) * circleRadius);
+            double yOffset = centerY + (Math.sin(angle) * circleRadius);
+
+            Circle point = new Circle(xOffset, yOffset, pointRadius);
+            point.setFill(Color.TRANSPARENT);
+            points.add(point);
+
+            drawing.getChildren().add(points.get(i));
+        }
+
+        
+        return points;
     }
 
+        //Method for making lines
+        /*Input: multVal: Times table value, points: Circle made from createCircle()
+                 drawing: Pane needed for displaying lines on GUI
+        */
+        public void createLines(Pane drawing, int multVal, ArrayList<Circle> points) {
+
+        if(lines.isEmpty() == false) {
+            lines.clear();
+        }
+        //Initialize variables
+        //Will store the position of the start and end points for each line
+        double lineStartX;
+        double lineStartY;
+        double lineEndX;
+        double lineEndY;
+
+        //Loop through
+        for(int i = 0; i < points.size(); i++) {
+
+            //Store starting point and get position
+            Circle pointStart = new Circle();
+            pointStart= points.get(i);
+            lineStartX = pointStart.getCenterX();
+            lineStartY = pointStart.getCenterY();
+            
+            Circle pointEnd = new Circle();
+            //if end value is > pointAmt, modulo to get final point
+            // else get point at the end value
+            if (i*multVal >= points.size()) {
+                //Store end point and get position
+                pointEnd = points.get(((i*multVal) % points.size()));
+                lineEndX = pointEnd.getCenterX();
+                lineEndY = pointEnd.getCenterY();
+            }
+            else {
+                pointEnd = points.get(i*multVal);
+                lineEndX = pointEnd.getCenterX();
+                lineEndY = pointEnd.getCenterY();
+            }
+
+            //Make new line
+            Line line = new Line();
+
+            //set start position
+            line.setStartX(lineStartX);
+            line.setStartY(lineStartY);
+
+            //set end position
+            line.setEndX(lineEndX);
+            line.setEndY(lineEndY);
+
+            //Set display properties
+            line.setStroke(Color.WHITE);
+
+            //Store and display lines
+            lines.add(line);
+            drawing.getChildren().add(lines.get(i));
+        }
+    }
+
+    //Start method for timesTable
     public void start() {
         AnimationTimer timer = new AnimationTimer() {
             private Duration lastUpdate = Duration.of(0, ChronoUnit.NANOS);
@@ -46,7 +118,7 @@ public class timesTable {
                 Duration nowDur = Duration.of(now, ChronoUnit.NANOS);
                 if (nowDur.minus(lastUpdate).toMillis() > 25) {
                     lastUpdate = nowDur;    
-                    generateTable(size);
+                  //  generateConnections(pointArray);
                 }
             }
         };
--- src/timesTableMain.java
+++ src/timesTableMain.java
@@ -1,37 +1,217 @@
+import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ChangeListener;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
 
 public class timesTableMain extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
+    private final int drawHeight = 550;
+    private final int drawWidth = 800;
+    private final int optSize = 50;
+    private final int pointMin = 10;
+    private final int pointMax = 360;
+
+    private int points;
+    private int multVal;
+    private ArrayList<Circle> circle = new ArrayList<>();
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Times Table Visualization");
 
-        double size = 800;
+        //Initialize Variables
+        //These variables cannot be listed as private; Illegal Parameter
 
+        //Box Initializtion
+        BorderPane root = new BorderPane();
+
+
+/* 
+* ---------------------------
+*        USER CONTROLS
+* --------------------------- 
+*/ 
         Pane drawing = new Pane();
+        VBox options = new VBox(10);
+        HBox pointInput = new HBox(8);
+        HBox valInput = new HBox(8);
+
+
+        //Text box input for setting points
+        Label pointLabel = new Label("Number of Points: ");
+        TextField pointsAmt = new TextField("10");
+        Button submitP = new Button("Submit");
+
+        //Slider for setting times table value;
+        Slider tableVal = new Slider(2,360,2);
+        tableVal.setShowTickMarks(true);
+        tableVal.setShowTickLabels(true);
+        tableVal.setBlockIncrement(0.1f);
+        Label valLabel = new Label("Times Table Value: ");
+        Label value = new Label(Double.toString(tableVal.getValue()));
+
+        //Time increment slider
+        //Increase in value increases jumped values
+        Slider incrementSlider = new Slider(1,10,1);
+        tableVal.setShowTickMarks(true);
+        tableVal.setShowTickLabels(true);
+        tableVal.setBlockIncrement(0.1f);
+        Label incLabel = new Label("T: ");
+        Label incValue = new Label(Double.toString(incrementSlider.getValue()));
+
+        //FPS slider
+        //Change delay between increments
+        Slider fpsSlider = new Slider(1,10,1);
+        tableVal.setShowTickMarks(true);
+        tableVal.setShowTickLabels(true);
+        tableVal.setBlockIncrement(0.1f);
+        Label fpsLabel = new Label("T: ");
+        Label fpsValue = new Label(Double.toString(fpsSlider.getValue()));
+
+
+        //Animation control
+        Button animCtrl = new Button("Play/Pause");
+
+/*
+ * ---------------------------
+ *       GUI DISPLAY
+ * ---------------------------
+ */
+        pointInput.getChildren().addAll(pointLabel,pointsAmt, submitP);
+        valInput.getChildren().addAll(valLabel, value);
+        
+        options.getChildren().addAll(valInput,tableVal,pointInput,animCtrl);
+
+
+
+        //Setup Pane for drawing
         drawing.setStyle("-fx-background-color: Black");
-        drawing.setMinWidth(size);
-        drawing.setMinHeight(size);
+        drawing.setMinWidth(drawWidth);
+        drawing.setMinHeight(drawHeight);
 
-        timesTable table = new timesTable();
+        //Set size for option pane from optSize
+        options.setMinHeight(optSize);
 
-        Scene scene = new Scene(drawing, size, size);
+        root.setCenter(drawing);
+        root.setTop(options);
+
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setMinHeight((drawHeight + optSize));
+        primaryStage.setMinWidth(drawWidth);
         primaryStage.show();
 
-        // int val = 5;
-       // int[][] mat = table.generateTable(val);
-        // Loop through all rows
-        //for (int i = 0; i < mat.length; i++)
- 
-            // Loop through all elements of current row
-          //  for (int j = 0; j < mat[i].length; j++)
-          //      System.out.print(mat[i][j] + " ");
+
+/*
+ * ------------------------------
+ *       CIRCLE ANIMATION
+ * ------------------------------
+ */
+        //Create instance of timesTable class
+        timesTable generator = new timesTable();
+
+         //Create circle in drawing pane
+        points = Integer.parseInt(pointsAmt.getText());
+        multVal = (int) Math.round(tableVal.getValue());
+        circle = generator.createCircle(drawing, points);
+        generator.createLines(drawing, multVal, circle);
+
+        //Event listener for multValue slider
+        tableVal.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(
+               ObservableValue<? extends Number> observableValue, 
+               Number oldValue, 
+               Number newValue) { 
+                  value.textProperty().setValue(
+                       String.format("%.1f", newValue.doubleValue()));
+
+                  drawing.getChildren().clear();
+                  circle = generator.createCircle(drawing, points);
+                  generator.createLines(drawing, newValue.intValue(), circle);
+              }
+        });
+
+        //Event listener for time increment slider
+        incrementSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(
+               ObservableValue<? extends Number> observableValue, 
+               Number oldValue, 
+               Number newValue) { 
+                  incValue.textProperty().setValue(
+                       String.format("%d", newValue.intValue()));
+
+                  
+              }
+        });
+
+        //Event listener for fps slider
+        fpsSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(
+               ObservableValue<? extends Number> observableValue, 
+               Number oldValue, 
+               Number newValue) { 
+                  fpsValue.textProperty().setValue(
+                       String.format("%d", newValue.intValue()));
+
+                  
+              }
+        });
+
+        //Event Listener for points input
+        //If input is greater than 360, output will default to 360
+        pointsAmt.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, 
+                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    pointsAmt.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        submitP.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+
+                if (Integer.parseInt(pointsAmt.getText()) > pointMax) {
+                    points = pointMax;
+                }
+                else if (Integer.parseInt(pointsAmt.getText()) < pointMin) {
+                    points = pointMin;
+                }
+                else {
+                points = Integer.parseInt(pointsAmt.getText());
+                }
+
+                drawing.getChildren().clear();
+                multVal = (int) Math.round(tableVal.getValue());
+                circle = generator.createCircle(drawing, points);
+                generator.createLines(drawing, multVal, circle);
+            }
+        });
+
     }
 }
